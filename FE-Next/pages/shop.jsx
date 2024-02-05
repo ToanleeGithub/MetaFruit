451c0b48a4ba4f6b6e86821bb6101ff7018f2270
Toanlee <nhattoanbds38@gmail.com>
2024-02-05 15:51:39 +0700

    update shop

--- FE-Next/pages/shop.jsx
+++ FE-Next/pages/shop.jsx
@@ -3,10 +3,19 @@ import { AppLayout } from "../components";
 import { FRUIT_ADDRESS } from "../addresses";
 import { useContract, useNFTs } from "@thirdweb-dev/react";
 import { NFT } from "../components";
+import Image from "next/image";
 
 const Shop = () => {
   const { data: fruitContract } = useContract(FRUIT_ADDRESS);
-  const { data: fruitNFTs } = useNFTs(fruitContract);
+  const { data: fruitNFTs, isLoading } = useNFTs(fruitContract);
+  if (isLoading)
+    return (
+      <AppLayout>
+        <div className="flex justify-center items-center h-[100%]">
+          <Image src="/loader.svg" alt="Loading" width={100} height={100} />
+        </div>
+      </AppLayout>
+    );
   return (
     <AppLayout>
       <div className="grid lg:grid-cols-3 md:grid-cols-2 gap-3">
